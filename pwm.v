a9d11d177debe800ea9effbc1e55ef2b9eda31e5
Ubiratan Tavares <ust1973@gmail.com>
2025-06-16 18:14:40 -0300

    implementação do módulo PWM

--- pwm.v
+++ pwm.v
@@ -6,4 +6,17 @@ module PWM (
     output reg pwm_out
 );
 
+	reg [15:0] pwm_counter;
+
+	always @(posedge clk or negedge rst_n) begin
+		if (!rst_n) begin // reset assíncrono, ativo em nível baixo
+			pwm_counter <= 16'h0;
+			pwm_out <= 1'b0;			
+		end else begin
+			// lógica de contagem
+			pwm_counter <= (pwm_counter >= (period - 1)) ? 16'h0 : pwm_counter + 1; 
+			// gera sinal PWM
+			pwm_out <= (pwm_counter < duty_cycle) ? 1'b1 : 1'b0; 
+		end
+	end
 endmodule
